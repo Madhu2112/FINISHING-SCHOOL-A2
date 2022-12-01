d5e4c200b4baa73285a7a7ee76e96e3255dcbd60
Madhu2112 <kothamadhuvani@gmail.com>
2022-12-01 13:27:28 +0530

    code

---
+++ 30 NOV/p3.java
@@ -0,0 +1,34 @@
+
+//leetcode 647
+import java.util.*;
+
+class sol {
+    public static int countsub(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // check even
+            count += pal(s, i, i);
+            // check odd
+            count += pal(s, i, i + 1);
+        }
+        return count;
+    }
+
+    public static int pal(String str, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+
+        return count;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(countsub(s));
+        sc.close();
+    }
+}
